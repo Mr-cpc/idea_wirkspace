de5bc46dc2e0428c651c428f3e2a0cccae3c24c9
mrcpc <615706431@qq.com>
2017-09-27 16:34:47 +0800

    交换的方式实现

--- src/bupt_2017_9_27/BeautifulArr.java
+++ src/bupt_2017_9_27/BeautifulArr.java
@@ -1,6 +1,7 @@
 package bupt_2017_9_27;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,8 +34,29 @@ public class BeautifulArr {
         return (num & 0x1) == 1;
     }
 
+    public static List<Integer> otherBeautiful(int n,int k) {
+        Integer[] res = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = i+1;
+        }
+        int j = n-1,i = 1;
+        while(k-- > 0) {
+            swap(res,j--,i);
+            i += 2;
+        }
+        return Arrays.asList(res);
+    }
+
+    private static void swap(Integer[] res, int i, int j) {
+        if(res[i] == res[j])
+            return ;
+        res[i] ^= res[j];
+        res[j] ^= res[i];
+        res[i] ^= res[j];
+    }
 
     public static void main(String[] args) {
         System.out.println(beautiful(2,1));
+        System.out.println(otherBeautiful(5,2));
     }
 }
