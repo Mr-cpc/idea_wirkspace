618063061893a0230642cb205dc59bf544d93fe3
mrcpc <615706431@qq.com>
2017-09-27 10:40:43 +0800

    最多修改一个形成非降数组

---
+++ src/bupt_2017_9_27/NoDesc.java
@@ -0,0 +1,38 @@
+package bupt_2017_9_27;
+
+/**
+ * Created by waiting on 2017/9/27.
+ */
+public class NoDesc {
+    public static boolean oneModify(int nums[]) {
+        if(nums.length == 0 || nums.length == 1 || nums.length == 2)
+            return true;
+        int minMax = nums[0],time = 0,copy[] = new int[nums.length+1];
+        for(int i = 1;i<copy.length;i++)
+            copy[i] = nums[i-1];
+        copy[0] = Integer.MIN_VALUE;
+        for(int i = 2;i<copy.length;i++) {
+            if(copy[i] >= copy[i-1])
+                continue;
+            else {
+                if(copy[i] >= copy[i-2]) {
+                    if(time != 0)
+                        return false;
+                    copy[i-1] = copy[i-2];
+                    time++;
+                }
+                else {
+                    copy[i] = copy[i-1];
+                    time++;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        int a[] = {1,2,1,9};
+        System.out.println(oneModify(a));
+    }
+}
