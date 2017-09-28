42b58105d44c02c2ed2d660cf97fe4092427b0d2
mrcpc <615706431@qq.com>
2017-09-28 17:39:56 +0800

    搞错题意了

--- src/bupt_2017_9_28/ValidPa2.java
+++ src/bupt_2017_9_28/ValidPa2.java
@@ -4,6 +4,7 @@ package bupt_2017_9_28;
  * Created by waiting on 2017/9/28.
  */
 public class ValidPa2 {
+    //搞错了，下面这个方法是针对限制为只能修改一个字符使得串回文，而不是删除一个字符的
     public static boolean va2(String str) {
         int len = str.length();
         StringBuilder sb = new StringBuilder(2 * len + 1).append('*');
@@ -23,7 +24,19 @@ public class ValidPa2 {
         return true;
     }
 
+
+    //
+    public static boolean va3(String s,int i, int j,int modifyTime) {
+        if(modifyTime >1 || i >= j)
+            return false;
+        if(s.charAt(i) == s.charAt(j))
+            return va3(s,i+1,j-1,modifyTime);
+        else {
+            return va3(s,i+1,j,modifyTime+1) || va3(s,i,j-1,modifyTime +1);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println(va2("abca"));
+        String s = "abca";
+        System.out.println(va3(s,0,s.length() - 1,0));
     }
 }
