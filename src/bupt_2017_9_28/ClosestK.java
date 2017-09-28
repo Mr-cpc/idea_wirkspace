a5a74ee0dc113c2d94e07f1f884f05d175ff904c
mrcpc <615706431@qq.com>
2017-09-28 09:57:00 +0800

    不需要数已经有多少个，只要看还差多少个到k

---
+++ src/bupt_2017_9_28/ClosestK.java
@@ -0,0 +1,64 @@
+package bupt_2017_9_28;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by waiting on 2017/9/28.
+ */
+public class ClosestK {
+
+    public static List<Integer> closestK(int nums[],int k,int x) {
+        List<Integer> res = new ArrayList<>(k);
+        if(x <= nums[0]) {
+            for(int i = 0;i<k;i++)
+                res.add(nums[i]);
+        }
+        else {
+            if(x >= nums[nums.length - 1]) {
+                for(int i = 1;i<=k;i++)
+                    res.add(nums[nums.length - i]);
+            }
+            else {
+                int idx = closest(nums,x),i = idx - 1,j =  idx + 1,count = 1;
+                res.add(nums[idx]);
+                while(count++ <= k && i >= 0 && j < nums.length) {
+                    if(x - nums[i] <= nums[j] -x ) {
+                        res.add(0,nums[i--]);
+                    }
+                    else {
+                        res.add(res.size(),nums[j++]);
+                    }
+                }
+                if(count > k)
+                    return res;
+                else {
+                    if(i < 0) {
+                        while(count++ <= k)
+                            res.add(nums[j++]);
+                    }
+                    else {
+                        while(count ++ <= k)
+                            res.add(0,nums[i--]);
+                    }
+                }
+            }
+        }
+        return  res;
+    }
+
+    public static int closest(int nums[],int x) {
+        for(int i = 0;i<nums.length - 1;i++) {
+            if(nums[i] < x && nums[i+1] >= x)
+                return nums[i+1] - x <= x - nums[i]? i+1:i;
+        }
+        return nums.length - 1;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1,5,8,9,13};
+        System.out.println(closest(a,0));
+        System.out.println(closestK(a,3,7));
+    }
+
+}
