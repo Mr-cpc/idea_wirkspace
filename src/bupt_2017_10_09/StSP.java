7ea2cb2253f5aff6e722b4b4ee3cff46323d3ee9
mrcpc <615706431@qq.com>
2017-10-10 15:42:43 +0800

    不是盲目的广搜，改成贪婪了

--- src/bupt_2017_10_09/StSP.java
+++ src/bupt_2017_10_09/StSP.java
@@ -1,71 +1,98 @@
 package bupt_2017_10_09;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by waiting on 2017/10/9.
  */
 public class StSP {
-    public static int stisp(String[] ws,String t) {
-        Map<Character,Integer> map = new HashMap<>(),maps[] = new Map[ws.length];
-        for(char ch:t.toCharArray())
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        for(int i = 0;i<ws.length;i++) {
+    public static int stisp(String[] ws, String t) {
+        Map<Character, Integer> map = new HashMap<>(), maps[] = new Map[ws.length];
+        for (char ch : t.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        for (int i = 0; i < ws.length; i++) {
             maps[i] = new HashMap<>();
-            for(char ch:ws[i].toCharArray()) {
-                maps[i].put(ch,maps[i].getOrDefault(ch,0)+1);
+            for (char ch : ws[i].toCharArray()) {
+                maps[i].put(ch, maps[i].getOrDefault(ch, 0) + 1);
             }
         }
         Queue<Status> q = new LinkedList<>();
-        q.offer(new Status<Character, Integer>(0,map) );
-        while(!q.isEmpty()) {
+        q.offer(new Status(0, map));
+        while (!q.isEmpty()) {
             Status cur = q.poll();
-            if(cur.map.size() == 0)
+            if (cur.map.size() == 0)
                 return cur.step;
             else {
-                for(Map<Character,Integer> m:maps) {
-                    Map<Character,Integer> copy = new HashMap<>(cur.map);
-                    boolean flag = false;
-                    for(Map.Entry<Character,Integer> me : m.entrySet()) {
-                        Character key = me.getKey();
-                        Integer val = me.getValue(),curVal = copy.get(key);
-                        if(curVal != null) {
-                            flag = true;
-                            if(curVal > val)
-                                copy.put(key,curVal - val);
-                            else
-                                copy.remove(key);
+                Map<Character, Integer> m = findClosest(cur.map, maps);
+//                    Map<Character,Integer> copy = new HashMap<>(cur.map);
+                for (Map.Entry<Character, Integer> me : m.entrySet()) {
+                    Character key = me.getKey();
+                    Integer val = me.getValue(), curVal = cur.map.get(key);
+                    if (curVal != null) {
+                        if (curVal > val)
+                            cur.map.put(key, curVal - val);
+                        else
+                            cur.map.remove(key);
 
-                        }
                     }
-                    if(flag)
-                        q.offer(new Status(cur.step+1,copy));
                 }
+                q.offer(new Status(cur.step + 1, cur.map));
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        String[] ws = {"with","example","science"};
+        String[] ws = {"with", "example", "science"};
         String t = "thehat";
         TimeStatistics.begin();
-        System.out.println(stisp(ws,t));
+        System.out.println(stisp(ws, t));
         System.out.println(TimeStatistics.end());
     }
+
+    public static Map<Character, Integer> findClosest(Map<Character, Integer> target, Map<Character, Integer>[] maps) {
+        int num = 0, max = 0;
+        Map<Character, Integer> ans = maps[0];
+        for (Map<Character, Integer> m : maps) {
+            num = 0;
+            for (Map.Entry<Character, Integer> me : target.entrySet()) {
+                if (m.get(me.getKey()) != null)
+                    num += me.getValue() <= m.get(me.getKey()) ? me.getValue() : m.get(me.getKey());
+            }
+            if (num >= max) {
+                ans = m;
+                max = num;
+            }
+
+        }
+        return ans;
+    }
+
 }
 
 
-
-class Status<K,V> {
+class Status {
     int step;
-    Map<K,V> map;
+    Map<Character, Integer> map;
 
-    public Status(int step, Map<K, V> map) {
+    public Status(int step, Map<Character, Integer> map) {
         this.step = step;
         this.map = map;
+    }
+}
+
+class MyPriorityQueue<T> {
+    PriorityQueue<T> q;
+
+    public MyPriorityQueue(Comparator<T> comparator) {
+        this.q = new PriorityQueue<T>(comparator);
+    }
+
+    public void setComparator(Comparator<T> comparator) {
+        PriorityQueue<T> t = q;
+        q = new PriorityQueue<T>(comparator);
+        while (!t.isEmpty()) {
+            q.add(t.poll());
+        }
     }
 }
