3ffc623388dc8d52e485c724e190dddd6535bdd6
mrcpc <615706431@qq.com>
2017-10-11 14:08:04 +0800

    批量梯度下降

---
+++ learnp/basic/lr/linear.py
@@ -0,0 +1,40 @@
+class Linear(object):
+    def __init__(self,params,points):
+        self.params = params[:]
+        self.points = points[:]
+
+    def set_learnrate(self,learn_rate):
+        self.learn_rate = learn_rate
+
+    def mul(self,a,b):
+        res = 0.0
+        if len(a) != len(b):
+            pass
+        else :
+            for i in range(len(a)):
+                res += a[i] * b[i]
+        return res
+
+    def compute_error(self):
+        error = 0
+        for i in range(len(self.points)):
+            feature = self.points[i][:len(self.points[i])-1]
+            error += (self.mul(self.params,feature) - self.points[i][-1]) ** 2
+        return error / (2 * len(points))
+
+    def gradient_desc(self):
+        for i in range(len(self.params)):
+            change = 0
+            for j in range(len(self.points)):
+                change += (self.mul(self.params,self.points[j][:len(self.points[j])-1]) - self.points[j][-1]) * self.points[j][i]
+            self.params[i] -= self.learn_rate * change
+params = [1,7]
+points = [[1,2,5],[1,3,8],[1,4,10]]
+linear = Linear(params,points)
+linear.set_learnrate(0.01)
+# print(points[0][:len(points[0])-1])
+for i in range(100):
+    print(linear.params)
+    print(linear.compute_error())
+
+    linear.gradient_desc()
