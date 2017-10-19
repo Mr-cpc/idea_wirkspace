55943a8f8e42e18346523299f6a655ea9ab74172
user.name <615706431@qq.com>
2017-10-19 21:37:09 +0800

    求逆写好了

--- learnp/basic/bupt_2017_10_19/mat.py
+++ learnp/basic/bupt_2017_10_19/mat.py
@@ -18,6 +18,17 @@ class Mat:
     def generateE(n):
         return [[1 if col == row else 0 for col in range(n)] for row in range(n)]
 
+    def leftMul(self,mat):
+        if len(self.mat[0]) != len(mat.mat):
+            raise Exception("the col not equals that row")
+        # for i in range(len())
+    def write(mat,filename):
+        with open(filename,"w") as f:
+            for row in mat:
+                for col in row:
+                    f.write(str(col))
+                    f.write(" ")
+                f.write("\n")
 
     def inv(self):
         if len(self.mat) == 0 or len(self.mat) != len(self.mat[0]):
@@ -25,11 +36,23 @@ class Mat:
         mat = Mat()
         E = Mat.generateE(len(self.mat))
         argumented = [self.mat[row]+E[row] for row in range(len(self.mat))]
-        for i in range(len(self.mat[0])-1):
-            for j in range(1,len(argumented)):
-                argumented[j] = [argumented[j][i] -  self.mat[i][j]/self.mat[i][i] * argumented[j][i] for i in range(len(argumented[j]))]
-        return [[argumented[row][col] for col in range(len(self.mat),len(argumented[0]))] for row in range(len(argumented))]
+        for i in range(len(self.mat)):
+            for j in range(i+1,len(self.mat)):
+                # for k in range(len(argumented[j])):
+                #     argumented[j][k] -= argumented[j][i] * argumented[i][k] /argumented[i][i]
+                argumented[j] = [(argumented[j][k] - argumented[j][i] * argumented[i][k] / argumented[i][i]) for k in range(len(argumented[j]))]
+        for i in list(range(len(self.mat)))[::-1]:
+            for j in list(range(i))[::-1]:
+                argumented[j] = [(argumented[j][k] - argumented[j][i] * argumented[i][k] / argumented[i][i]) for k in range(len(argumented[j]))]
+        for i in range(len(argumented)):
+            for j in range(len(argumented[i])):
+                argumented[i][j] /= argumented[i][i]
+        Mat.write(argumented,"argu.txt")
+        mat.mat = [[argumented[row][col] for col in range(len(self.mat),len(argumented[0]))] for row in range(len(argumented))]
+        return mat
 
+# with open("mat.txt","w") as f:
+#     for i in range(3):
+#         f.write("1 2 3\n")
 mat = Mat("mat.txt")
-print(mat.mat)
-print(mat.inv())
+print(mat.inv().mat)
