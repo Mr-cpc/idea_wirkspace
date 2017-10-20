d8a8e35a8e1c947e1ca69186425e423fafb7f907
mrcpc <615706431@qq.com>
2017-10-20 11:22:50 +0800

    inv和transpose写好了

--- learnp/basic/bupt_2017_10_19/mat.py
+++ learnp/basic/bupt_2017_10_19/mat.py
@@ -16,12 +16,26 @@ class Mat:
 
 
     def generateE(n):
-        return [[1 if col == row else 0 for col in range(n)] for row in range(n)]
+        mat =  Mat()
+        mat.mat = [[1 if col == row else 0 for col in range(n)] for row in range(n)]
+        return mat
 
-    def leftMul(self,mat):
-        if len(self.mat[0]) != len(mat.mat):
+    def leftMul(self,r_mat):
+        if len(self.mat[0]) != len(r_mat.mat):
             raise Exception("the col not equals that row")
-        # for i in range(len())
+        mat = Mat()
+        '''
+        # mat.mat = [[0] * len(r_mat.mat[0])] * len(self.mat) !!!don't use this way to create a 2D Array
+        '''
+        mat.mat = [[0 for i in range(len(r_mat.mat[0]))] for j in range(len(self.mat)) ]
+        print(mat.mat)
+        for i in range(len(self.mat)):
+            for j in range(len(r_mat.mat[0])):
+                for k in range(len(r_mat.mat)):
+                    # print(str(k)+"mat["+str(i)+"]"+"["+str(j)+"]"+":"+str(mat.mat[i][j]))
+                    mat.mat[i][j] += self.mat[i][k] * r_mat.mat[k][j]
+        # mat.mat = [list(map(lambda x,y:x * y,self.mat[i],[mat[j][i] for j in len(mat)] )) for i in range(len(self.mat))]
+        return mat
     def write(mat,filename):
         with open(filename,"w") as f:
             for row in mat:
@@ -34,7 +48,7 @@ class Mat:
         if len(self.mat) == 0 or len(self.mat) != len(self.mat[0]):
             raise Exception("not a phalanx!")
         mat = Mat()
-        E = Mat.generateE(len(self.mat))
+        E = Mat.generateE(len(self.mat)).mat
         argumented = [self.mat[row]+E[row] for row in range(len(self.mat))]
         for i in range(len(self.mat)):
             for j in range(i+1,len(self.mat)):
@@ -45,8 +59,9 @@ class Mat:
             for j in list(range(i))[::-1]:
                 argumented[j] = [(argumented[j][k] - argumented[j][i] * argumented[i][k] / argumented[i][i]) for k in range(len(argumented[j]))]
         for i in range(len(argumented)):
-            for j in range(len(argumented[i])):
-                argumented[i][j] /= argumented[i][i]
+            argumented[i] = [argumented[i][j] / argumented[i][i] for j in range(len(argumented[i]))]
+            # for j in range(len(argumented[i])):
+            #     argumented[i][j] /= argumented[i][i]
         Mat.write(argumented,"argu.txt")
         mat.mat = [[argumented[row][col] for col in range(len(self.mat),len(argumented[0]))] for row in range(len(argumented))]
         return mat
@@ -55,4 +70,6 @@ class Mat:
 #     for i in range(3):
 #         f.write("1 2 3\n")
 mat = Mat("mat.txt")
-print(mat.inv().mat)
+inv = mat.inv()
+Mat.write(inv.mat,"inv.txt")
+print(inv.leftMul(mat).mat)
