875924454b8c680fed087680dcf8324aa9d8f63b
mrcpc <615706431@qq.com>
2017-10-23 11:29:58 +0800

    加入施密特正交

--- learnp/basic/bupt_2017_10_19/mat.py
+++ learnp/basic/bupt_2017_10_19/mat.py
@@ -1,16 +1,31 @@
 import re
-
+from functools import reduce
 from basic.bupt_2017_10_20.ColVec import ColVec
 from basic.bupt_2017_10_20.RowVec import RowVec
 
 
 class Mat:
-    def __init__(self,filepath=""):
-            try:
-                with open(filepath) as f:
-                    self.mat = [[float(num) for num in re.split("\\s+",line[:-1])] for line in f.readlines()]
-            except FileNotFoundError:
-                self.mat = []
+    def __init__(self,filepath="",mat=None,ndarray=None):
+        if filepath != "":
+            self.init_by_file(filepath)
+        elif mat != None:
+            self.init_by_mat(mat)
+        elif ndarray != None:
+            self.init_by_ndarray(ndarray)
+        else:
+            self.mat = []
+
+    def init_by_file(self,filepath):
+        try:
+            with open(filepath) as f:
+                self.mat = [[float(num) for num in re.split("\\s+",line[:-1])] for line in f.readlines()]
+        except FileNotFoundError:
+            self.mat = []
+    def init_by_mat(self, mat):
+        self.mat = mat.mat[:]
+
+    def init_by_ndarray(self, ndarray):
+        self.mat = ndarray[:]
 
     def transpose(self):
         mat = Mat()
@@ -58,7 +73,24 @@ class Mat:
     def getCol(self,n):
         return ColVec([self.mat[row][n] for row in range(len(self.mat))])
 
+    def dotprod(v1,v2):
+        return reduce(lambda x,y:x+y,map(lambda x,y:x*y,v1,v2))
 
+    def list_num_mul(v,num):
+        return [ele * num for ele in v]
+    def list_sub_list(v1,v2):
+        for i in range(len(v1)):
+            v1[i] -= v2[i]
+    def orthogonal(self):
+        ort = Mat()
+        ort.mat = [[0 for i in range(len(self.mat[0]))] for j in range(len(self.mat))]
+        for row in range(len(self.mat)):
+            ort.mat[row] = self.mat[row][:]
+            print(ort.mat[row])
+            for i in range(row):
+                Mat.list_sub_list(ort.mat[row],Mat.list_num_mul(ort.mat[i],Mat.dotprod(self.mat[row],ort.mat[i])/Mat.dotprod(ort.mat[i],ort.mat[i])))
+            print(ort.mat[row])
+        return ort
     def inv(self):
         if len(self.mat) == 0 or len(self.mat) != len(self.mat[0]):
             raise Exception("not a phalanx!")
@@ -86,7 +118,8 @@ class Mat:
 # with open("mat.txt","w") as f:
 #     for i in range(3):
 #         f.write("1 2 3\n")
-# mat = Mat("mat.txt")
+mat = Mat("mat.txt")
 # inv = mat.inv()
 # Mat.write(inv.mat,"inv.txt")
 # print(mat.rightMul(inv).mat)
+print(mat.orthogonal().mat)
