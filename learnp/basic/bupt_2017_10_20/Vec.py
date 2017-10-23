87ea93a0a68be8b067210c68b103c92410696870
mrcpc <615706431@qq.com>
2017-10-23 15:14:15 +0800

    换下位置

--- learnp/basic/bupt_2017_10_20/Vec.py
+++
@@ -1,7 +0,0 @@
-import abc
-class Vec:
-    def __init__(self,data):
-        self.data = data[:]
-
-    @abc.abstractmethod
-    def mul(self,vec):pass
--- learnp/basic/bupt_2017_10_20/mattest.py
+++ learnp/basic/bupt_2017_10_20/mattest.py
@@ -1,11 +1,11 @@
 from basic.bupt_2017_10_19.mat import Mat
 from basic.bupt_2017_10_20.ColVec import ColVec
 from basic.bupt_2017_10_20.RowVec import RowVec
+from basic.bupt_2017_10_20.Vec import Vec
 
 u = Mat("u.txt")
 v = Mat("v.txt")
 s = Mat("E.txt")
 
-u_col = u.getCol(0)
-v_col = v.getCol(0)
-print(s.leftMul(Mat(v_col.data)))
+vecs = [vec.norm() for vec in  Vec.orthogonal(Mat("mat.txt").rows())]
+print(vecs[2])
