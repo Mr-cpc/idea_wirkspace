bc08824b248a632672216ca1f267bb965e3faa91
mrcpc <615706431@qq.com>
2017-10-27 17:37:32 +0800

    先存下，

---
+++ learnp/basic/bupt_2017_10_27/subaprodlek.py
@@ -0,0 +1,26 @@
+def subaprodlek(nums,k):
+    prefix = [1]*(len(nums) +1)
+    res = []
+    for i in range(1,len(prefix)):
+        prefix[i] = prefix[i-1] * nums[i-1]
+    res = [nums[i:j] for i in range(len(prefix)) for j in range(i+1,len(prefix)) if prefix[j] / prefix[i] < k]
+    return res
+
+def slide(nums,k):
+
+    # i, j = 0, 0
+    pro ,l,r= 1,0,0
+    res = 0
+    while r < len(nums):
+        pro *= nums[r]
+        r+=1
+        while pro > k:
+            pro /= nums[l]
+            l+=1
+        res += r - l +1
+    return res
+    #
+    # pass
+res = subaprodlek([10,5,2,6],100)
+print(slide([10,5,2,6],100))
+print(len(res))
