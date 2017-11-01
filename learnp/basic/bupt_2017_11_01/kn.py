e0168ea4c29d2b352174abf7a514fca431159896
mrcpc <615706431@qq.com>
2017-11-01 11:33:15 +0800

    knn，先存一下，还没用数据集测试

---
+++ learnp/basic/bupt_2017_11_01/kn.py
@@ -0,0 +1,37 @@
+from math import sqrt
+import heapq
+from basic.bupt_2017_10_19.Vec import Vec
+
+
+class kn:
+    def __init__(self,datas:list,k:int):
+        self.datas = [Vec(data[:-1]).norm() for data in datas]
+        self.lab = {self.datas[i]:datas[i][-1] for i in range(len(datas))}
+        self.k = int(sqrt(len(datas))) if k > sqrt(len(datas)) else k
+
+    def predict(self,sam:list):
+        sam = Vec(sam)
+        dis_dict = {}
+        for data in self.datas:
+            dis = sam.distance(data)
+            if dis not in dis_dict:
+                dis_dict[dis] = [data]
+            else :
+                dis_dict[dis].append(data)
+        largest_dis = heapq.nlargest(self.k,dis_dict.keys())
+        kn_datas = []
+        for dis in largest_dis:
+            kn_datas.extend(dis_dict[dis])
+            if len(kn_datas) >= self.k:
+                break
+        fre = {}
+        for data in kn_datas:
+            fre[self.lab[data]] = fre.get(self.lab[data],0)+1
+        lab,max_fre = None,0
+        for k in fre:
+            if fre[k] > max_fre:
+                lab,max_fre = k,fre[k]
+        return lab
+
+
+
