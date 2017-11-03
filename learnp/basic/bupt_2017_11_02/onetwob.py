a020ee0822da9e16ef36ac7ad4be99f380781e7d
user.name <615706431@qq.com>
2017-11-03 20:12:43 +0800

    经过testcase，这个是ac的，108ms

--- learnp/basic/bupt_2017_11_02/onetwob.py
+++ learnp/basic/bupt_2017_11_02/onetwob.py
@@ -12,7 +12,7 @@ def check(bits:list):
     elif len(bits) == 1:
         return bits[0] == 0
     elif len(bits) == 2:
-        return bits[0] == 1 and bits[1] in (0,1)
+        return bits == [0,0] or (bits[0] == 1 and bits[1] in (0,1))
     else:
         if bits[0] == 0:
             return check(bits[1:])
@@ -22,13 +22,13 @@ def check(bits:list):
 
 def isone(bits:list):
     if len(bits) < 2:
-        return bits[0] == 1
+        return bits[0] == 0
     else:
         if bits[-2] == 1:
             return not check(bits[:-2])
         else:
             return check(bits[:-1])
 
-bits = [1,1,0]
+bits = [0,0,0]
 
 print(isone(bits))
