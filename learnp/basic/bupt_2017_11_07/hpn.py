2921dfcb398ef398ec2d8c91eff93e9ebe46c591
user.name <615706431@qq.com>
2017-11-07 19:08:31 +0800

    有可能是一个序列，而不仅仅是上一次，所以需要用hash来存储

--- learnp/basic/bupt_2017_11_07/hpn.py
+++ learnp/basic/bupt_2017_11_07/hpn.py
@@ -1,12 +1,16 @@
 def ishpn(n:int) ->bool:
+    s = set()
     while True:
         nums = [int(x) for x in str(n)]
         last_n = n
+        s.add(n)
         n = sum(num ** 2 for num in nums)
         if n == 1:
             break
         elif n == last_n:
             return False
+        elif n in s:
+            return False
     return True
 
-print(ishpn(19))
+print(ishpn(2))
