3ccb0e385ddc6443a78be41e2547b45f88f4968f
user.name <615706431@qq.com>
2017-11-07 20:53:50 +0800

    看错了，题目要求是小时不要前导0,分钟要前导0,这一题居然56ms，目前最快的记录也才59ms，看来python用户大部分是用的py2吧

--- learnp/basic/bupt_2017_11_07/biwa.py
+++ learnp/basic/bupt_2017_11_07/biwa.py
@@ -33,17 +33,18 @@ def get_bin_time_str(h:list,time:list):
     return res
 
 
-def biwa(n:int)->list:
+def biwa(num:int)->list:
     h = [0] * 4
     m = [0] * 6
     res = []
-    for i in range(0,n+1):
+    for i in range(0,num+1):
         num_h = get_bin_time_str(com(4,i),h)
-        num_m = get_bin_time_str(com(6,n-i),m)
+        num_m = get_bin_time_str(com(6,num-i),m)
         for hour in num_h:
             for mte in num_m:
                 hour_dec,mte_dec = int(hour,2),int(mte,2)
                 if hour_dec < 12 and mte_dec < 60:
-                    res.append("{}:{}".format(str(hour_dec).zfill(2),str(mte_dec).zfill(2)) )
+                    res.append("{}:{}".format(hour_dec,str(mte_dec).zfill(2)) )
     return res
 
+print(biwa(0))
