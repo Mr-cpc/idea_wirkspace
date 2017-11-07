c1d2741cc3eca566e9ab87cd7965a9917b515c4b
mrcpc <615706431@qq.com>
2017-11-07 09:50:28 +0800

    先把循环的写上，我记得是有公式直接判断的

---
+++ learnp/basic/bupt_2017_11_07/hpn.py
@@ -0,0 +1,13 @@
+def ishpn(n:int) ->bool:
+    last_n = n
+    while True:
+        nums = [int(x) for x in str(n)]
+        last_n = n
+        n = sum(num ** 2 for num in nums)
+        if n == 1:
+            break
+        elif n == last_n:
+            return False
+    return True
+
+print(ishpn(19))
