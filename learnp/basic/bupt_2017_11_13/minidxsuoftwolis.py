c53ad1f5e2d0e7dbcd328882bf9b9a31c8244a13
mrcpc <615706431@qq.com>
2017-11-13 15:55:36 +0800

    感觉现在对于大部分easy都依赖于用map来解决

---
+++ learnp/basic/bupt_2017_11_13/minidxsuoftwolis.py
@@ -0,0 +1,10 @@
+def miidxsuoftwolis(list1:list,list2:list) ->list:
+    d1 = {key:idx for idx,key in enumerate(list1)}
+    d2 = {key:idx for idx,key in enumerate(list2)}
+    d3 = {key:d1[key] + d2[key] for key in d1 if key in d2}
+    min_v = len(list1) + len(list2)
+    for key in d3:
+        min_v = min(min_v,d3[key])
+    for key in d3:
+        min_v = min(min_v,d3[key])
+    return [key for key in d3 if d3[key] == min_v]
