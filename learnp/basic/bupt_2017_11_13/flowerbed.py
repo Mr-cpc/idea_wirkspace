07e89305fa9813dce37c3cc75ead9f542eb84acf
mrcpc <615706431@qq.com>
2017-11-13 16:39:07 +0800

    没什么问题，判断条件还是单独用一个函数比较好想

---
+++ learnp/basic/bupt_2017_11_13/flowerbed.py
@@ -0,0 +1,23 @@
+def left_can_place(i, flowerbed):
+    return i - 1 < 0 or flowerbed[i-1] == 0
+
+
+def rigth_can_place(i, flowerbed):
+    return i + 1 >= len(flowerbed) or flowerbed[i+1] == 0
+
+
+def flower(flowerbed:list,n:int) ->bool:
+    for i in range(len(flowerbed)):
+        if n == 0:
+            return True
+        elif flowerbed[i] == 1:
+            continue
+        else:
+            if left_can_place(i,flowerbed) and rigth_can_place(i,flowerbed):
+                n -= 1
+            else:
+                continue
+
+    return False
+
+print(flower([1,0,0,0,1],2))
