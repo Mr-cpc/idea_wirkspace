e398062b15663966bc29807ed1ba1f8f55feccc6
user.name <615706431@qq.com>
2017-11-21 20:08:13 +0800

    先改成这样

--- learnp/basic/bupt_2017_11_21/secmiv.py
+++ learnp/basic/bupt_2017_11_21/secmiv.py
@@ -1,8 +1,11 @@
-from basic.bupt_2017_11_09.bstm import TreeNode
 from queue import Queue
+
+from idea_wirkspace.learnp.basic.bupt_2017_11_09.bstm import TreeNode
 
 
 def trav(root: TreeNode, val) -> int:
+    if root is None :
+        return None
     q = Queue()
     q.put(root)
     while not q.empty():
@@ -17,13 +20,20 @@ def trav(root: TreeNode, val) -> int:
 
 
 def secmiv(root: TreeNode) -> int:
-    if root is None:
-        return -1
-    elif root.left == root.right is None:
+    if root is None or (None not in (root.left,root.right) and root.left.val == root.right.val):
         return -1
     else:
         if root.left != root.right:
-            return max(root.left, root.right)
+            return max(root.left.val, root.right.val)
         else:
             l, r = trav(root.left, root.val), trav(root.right, root.val)
             return min(l, r) if l is not None and r is not None else -1 if l == r is None else l or r
+a = TreeNode(2)
+b = TreeNode(2)
+c = TreeNode(2)
+# a.left = b
+# a.right = c
+print(secmiv(a))
+a = float("inf")
+b = float("inf")
+print(a == b)
