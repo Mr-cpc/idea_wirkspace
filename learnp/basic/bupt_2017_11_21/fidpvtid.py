ab71e45052f106810e1d740f3241e79019d8ce54
user.name <615706431@qq.com>
2017-11-21 20:16:23 +0800

    最朴素的反而是最能解决的

--- learnp/basic/bupt_2017_11_21/fidpvtid.py
+++ learnp/basic/bupt_2017_11_21/fidpvtid.py
@@ -2,7 +2,7 @@ def fidpvtid(nums:list)->int:
     prefix = [0] * (len(nums) + 1)
     for i in range(1,len(prefix)):
         prefix[i] = prefix[i-1] + nums[i-1]
-    for i in range(1,len(nums)-1):
+    for i in range(len(nums)):
         if prefix[i] == prefix[-1] - prefix[i+1]:
             return i
     else:
