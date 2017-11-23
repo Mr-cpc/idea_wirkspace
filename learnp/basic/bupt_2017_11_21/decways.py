70a1d06c4b0088eafdfd837e8477e3e680fa9a57
user.name <615706431@qq.com>
2017-11-23 18:55:37 +0800

    最终版本正确性应该没问题，可惜ac不了，特别注意到之前的陷阱,int("01")结果是1，1在set里，但是01实际上是不在的，所以set还是以str的形式存放才比较安全

--- learnp/basic/bupt_2017_11_21/decways.py
+++ learnp/basic/bupt_2017_11_21/decways.py
@@ -1,10 +1,13 @@
+from basic.bupt_2017_10_30.deco import time_statistic
+
+
 def dfs(s:str, encoder_set:dict):
     if len(s) == 0:
         return 1
     elif len(s) == 1:
         return 1 if s != "0" else 0
     else:
-        if int(s[:2]) not in encoder_set:
+        if s[:2] not in encoder_set:
             if s[0] == "0":
                 return 0
             else:
@@ -12,10 +15,10 @@ def dfs(s:str, encoder_set:dict):
         else:
             return dfs(s[2:],encoder_set) + (dfs(s[1:],encoder_set) if s[0] != "0" else 0)
 
-
+@time_statistic
 def decways(s:str) -> int:
-    encoder_set = {num + 1 : chr(num+ord('A')) for num in range(26)}
+    encoder_set = {str(num + 1) : chr(num+ord('A')) for num in range(26)}
     return dfs(s,encoder_set)
 
 
-print(decways("100"))
+print(decways("4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948"))
