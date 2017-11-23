507c06c178a5eb9bfb758a2f46d7893305fa3d25
user.name <615706431@qq.com>
2017-11-23 20:30:24 +0800

    不要忘了不满足的时候直接break,从下一个开始..这是今天第二个ac的，medium的题目果然还是ac难度很高的

--- learnp/basic/bupt_2017_11_23/mxsqa.py
+++ learnp/basic/bupt_2017_11_23/mxsqa.py
@@ -1,28 +1,30 @@
-def check(mt, i_st, j_st, i_en, j_en):
+def check(matrix, i_st, j_st, i_en, j_en):
     for i in range(i_st, i_en + 1):
-        if mt[i][j_en] == 0:
+        if matrix[i][j_en] == "0":
             return False
     for j in range(j_st, j_en + 1):
-        if mt[i_en][j] == 0:
+        if matrix[i_en][j] == "0":
             return False
     return True
 
 
-def mxsqa(mt: list) -> int:
+def mxsqa(matrix: list) -> int:
     ans = 0
-    for i in range(len(mt)):
-        for j in range(len(mt[0])):
-            if mt[i][j] == 0:
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 0:
                 continue
             else:
                 x, y = 0, 0
-                while i + x < len(mt) and j + y < len(mt[0]) and mt[i + x][j] == mt[i][j + y] == 1:
-                    if check(mt, i, j, i + x, j + y):
+                while i + x < len(matrix) and j + y < len(matrix[0]) and matrix[i + x][j] == matrix[i][j + y] == "1":
+                    if check(matrix, i, j, i + x, j + y):
                         ans = max(ans, (x + 1) * (x + 1))
-                    x += 1
-                    y += 1
+                        x += 1
+                        y += 1
+                    else:
+                        break
     return ans
 
 
-# print(mxsqa([[1,0,1,0,0],[1,0,1,1,1],[1,1,1,1,1],[1,0,1,1,1],]))
+print(mxsqa([["1","0","1","1","0","1"],["1","1","1","1","1","1"],["0","1","1","0","1","1"],["1","1","1","0","1","0"],["0","1","1","1","1","1"],["1","1","0","1","1","1"]]))
 print(mxsqa([[]]))
