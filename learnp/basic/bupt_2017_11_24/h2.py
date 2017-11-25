1925f8110389c39de8d069af3b4a58c1d3703aac
user.name <615706431@qq.com>
2017-11-25 11:26:31 +0800

    这是ac的版本，感觉真的智商不够用啊

--- learnp/basic/bupt_2017_11_24/h2.py
+++ learnp/basic/bupt_2017_11_24/h2.py
@@ -1,14 +1,15 @@
 def h2(citations:list) -> int:
+    if [0] * len(citations) == citations: #下面的二分解决不了全0的情况，这里单独判断
+        return 0
     l,r = 0 ,len(citations)-1
     while l < r:
         mid = l + ((r-l) >> 1)
-        if citations[mid] >= len(citations) - mid:
-            if mid - 1 >= 0 and citations[mid - 1] < citations[mid]:
-                return len(citations) - mid
-            else:
-                r = mid - 1
-        else:
+        if citations[mid] == len(citations) - mid:
+            return len(citations) - mid
+        elif citations[mid] < len(citations) - mid:
             l = mid + 1
+        else:
+            r = mid #为什么不是r = mid-1,是为了像[1,2,4,9,10]这样的输入mid不被跳过
     return len(citations) - l
 
-print(h2([8]))
+print(h2([0,0]))
