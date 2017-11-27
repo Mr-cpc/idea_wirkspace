83f1d1dfa6775e6737030d19ad1c62ad1ce0fa22
mrcpc <615706431@qq.com>
2017-11-27 14:18:19 +0800

    ？

--- learnp/basic/bupt_2017_11_27/bupt_login.py
+++ learnp/basic/bupt_2017_11_27/bupt_login.py
@@ -1,3 +1,8 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from basic.bupt_2017_11_24.login import Login
 
 lg = Login()
