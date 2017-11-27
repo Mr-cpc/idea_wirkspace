0e7d62592eabc8faa196baa7631fd63e08cf23eb
mrcpc <615706431@qq.com>
2017-11-27 10:53:49 +0800

    原来是form的post中少了一个字段，所以一直登录不成功

--- learnp/basic/bupt_2017_11_24/login.py
+++ learnp/basic/bupt_2017_11_24/login.py
@@ -2,22 +2,23 @@ from pip._vendor import requests
 
 
 class Login:
-    def __init__(self,login_url,**kwargs):
+    def __init__(self,data:dict = {"DDDDD":"2016140606","upass":"046614","0MKKey":""},login_url:str = "http://10.3.8.211",logout_url:str = "http://10.3.8.211/F.htm"):
         self.login_url = login_url
-        # self.data = {kwargs["uname_name"]:kwargs["uname_val"],kwargs["passwd_name"]:kwargs["passwd_val"]}
-        self.data = {"DDDDD":"2016140606","upass":"046614"}
-        self.cookie = {"myusername":"2016140606", "username":"2016140606", "smartdot":"046614"}
-        self.headers = {
+        self.data = data
+        # self.data = {"DDDDD":"2016140606","upass":"046614","0MKKey":""}
+        # self.cookie = {"myusername":"2016140606", "username":"2016140606", "smartdot":"046614"}
+        self.login_headers = {
             "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-             "Content-Type":"application/x-www-form-urlencoded"
+            "Content-Type":"application/x-www-form-urlencoded",
+            "Referer":"http://10.3.8.211/"
         }
+        self.logout_url = logout_url
+        self.session = requests.session()
     def login(self):
-        sesssion = requests.session()
-        # requests.post(self.login_url,)
-        sesssion.post(self.login_url,self.data,cookies = self.cookie,headers = self.headers)
-        r = sesssion.get(self.login_url)
+        r = self.session.post(self.login_url,self.data,headers = self.login_headers)
         print(r.text)
 
-
-lg = Login("http://10.3.8.211/",**{"uname_name":"DDDDD","passwd_name":"upass","uname_val":"2016140606","passwd_val":"046614"})
-lg.login()
+    def logout(self):
+        print(self.logout_url)
+        r = self.session.get(self.logout_url,headers = self.login_headers)
+        print(r.text)
