96b37fd31e1e50b95ce7f965d439cb62e7b7e77f
mrcpc <615706431@qq.com>
2017-12-04 17:06:06 +0800

    改成更pythonic的写法

--- learnp/basic/bupt_2017_12_04/Property.py
+++ learnp/basic/bupt_2017_12_04/Property.py
@@ -1,6 +1,5 @@
 import re
 
-
 '''
 User:waiting
 Date:2017-12-04
@@ -9,16 +8,11 @@ Time:14:20
 
 
 class Property:
-    def __init__(self, path: str,sep: str = '='):
+    def __init__(self, path: str, sep: str = '='):
         self.d = {}
         with open(path) as f:
-            key_vals = f.readlines()
-            for key_val in key_vals:
-                key_val = re.sub(r'\s+', '', key_val)
-                if key_val != '':
-                    key_val = key_val.split(sep)
-                    self.d[key_val[0]] = key_val[1]
+            key_vals = map(lambda x: re.sub(r'\s+', '', x), f.readlines())
+            self.d = {key_val.split(sep)[0]: key_val.split(sep)[1] for key_val in key_vals if key_val != ''}
 
     def __getitem__(self, item):
         return self.d[item]
-
