3b54d444bb62595ffa58f1934687207945adac7a
mrcpc <615706431@qq.com>
2018-01-02 15:20:11 +0800

    759. Set Intersection Size at least two. 还是使用以区间尾排序的方法。尽可能的使得排序后的相邻的区间交集更多

---
+++ learnp/basic/bupt_2018_1_2/intersectionatleasttwo.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from basic.bupt_2017_11_28.type_deco import prt
+import joblib
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from basic.bupt_2017_11_28.type_deco import prt
+import seaborn as sns
+
+'''
+User:waiting
+Date:2018-01-02
+Time:14:44
+'''
+def atleatwo(intervals:list) ->int:
+    intervals.sort(key=lambda itv:(itv[1],itv[0]))
+    ans = 2
+    prev_end = intervals[0][-1]
+    for i in range(1,len(intervals)):
+        if intervals[i][0] == prev_end:
+            ans += 1
+            prev_end = intervals[i][-1]
+        elif intervals[i][0] < prev_end:
+            pass
+        else:
+            ans += 2
+            prev_end = intervals[i][-1]
+    return ans
+print(atleatwo(intervals =  [[1, 2], [2, 3], [2, 4], [4, 5]]))
