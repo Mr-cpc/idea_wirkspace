106e499160fff42733322d8a57ab2896297ec195
mrcpc <615706431@qq.com>
2018-01-03 10:48:28 +0800

    这一次很有可能过了，少了一个记录的变量，不仅要记录上一个的尾，还要记录上一个的头

--- learnp/basic/bupt_2018_1_2/intersectionatleasttwo.py
+++ learnp/basic/bupt_2018_1_2/intersectionatleasttwo.py
@@ -18,16 +18,21 @@ def atleatwo(intervals:list) ->int:
     intervals.sort(key=lambda itv:(itv[1],itv[0]))
     print(intervals)
     ans = 2
-    prev_end = intervals[0][-1]
+    prev_one,prev_two = intervals[0]
     for i in range(1,len(intervals)):
-        if intervals[i][0] == prev_end:
+        if intervals[i][0] == prev_two:
             ans += 1
-            prev_end = intervals[i][-1]
-        elif intervals[i][0] < prev_end:
-            pass
+            prev_one,prev_two = intervals[i]
+        elif intervals[i][0] < prev_two:
+            if prev_one < intervals[i][0]:
+                ans += 1
+                prev_one,prev_two = prev_two,intervals[i][-1]
+            else:
+                prev_one,prev_two = prev_two-1,prev_two
+                pass
         else:
             ans += 2
-            prev_end = intervals[i][-1]
+            prev_one,prev_two = intervals[i]
     return ans
 print(atleatwo(intervals =  [[8,9],[4,21],[3,19],[5,9],[1,5]]))
 # std 4
