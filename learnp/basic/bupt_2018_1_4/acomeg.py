a91738c0ef3c57934a8066733a09ff65c3033096
user.name <615706431@qq.com>
2018-01-04 18:59:30 +0800

    目前这个case过不了，明天在看吧

--- learnp/basic/bupt_2018_1_4/acomeg.py
+++ learnp/basic/bupt_2018_1_4/acomeg.py
@@ -40,4 +40,4 @@ def acountmeg(accounts:list):
             cur.extend(sorted(list(s)))
             ans.append(cur)
     return ans
-print(acountmeg([["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]))
+print(acountmeg([["David","David0@m.co","David1@m.co"],["David","David3@m.co","David4@m.co"],["David","David4@m.co","David5@m.co"],["David","David2@m.co","David3@m.co"],["David","David1@m.co","David2@m.co"]]))
