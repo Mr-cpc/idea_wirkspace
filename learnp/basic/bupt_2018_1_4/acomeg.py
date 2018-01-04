9fbb0c11bfbac8eee74ff874c2a9a11e881442e7
mrcpc <615706431@qq.com>
2018-01-04 15:32:22 +0800

    729. Accounts Merge。 本来是比较麻烦的，但是用py写竟然能如此简洁。还有union-find的用法。即便如此，还是感觉会TLE

---
+++ learnp/basic/bupt_2018_1_4/acomeg.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from basic.bupt_2017_11_28.type_deco import prt
+import joblib
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from basic.bupt_2017_11_28.type_deco import prt
+import seaborn as sns
+
+'''
+User:waiting
+Date:2018-01-04
+Time:14:47
+'''
+def acountmeg(accounts:list):
+    from collections import defaultdict
+    d = defaultdict(list)
+    for account in accounts:
+        d[account[0]].append(account[1:])
+    ans = []
+    # print(d)
+    from basic.bupt_2017_12_29.delandear import UF
+    for name,accountss in d.items():
+        uf = UF(accountss)
+        for i in range(len(accountss)):
+            for j in range(i+1,len(accountss)):
+                if set(accountss[i]) & set(accountss[j]):
+                    uf.union(i,j)
+        d_ = defaultdict(list)
+        for i,value in enumerate(uf.id):
+            d_[value].append(accountss[i])
+        # print(d_)
+        for value in d_.values():
+            cur = [name]
+            s = set()
+            for accounts in value:
+                s |= set(accounts)
+            cur.extend(sorted(list(s)))
+            ans.append(cur)
+    return ans
+print(acountmeg([["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]))
