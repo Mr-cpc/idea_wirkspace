9fd06531561a07bebe8d36ca44b1d5f0e8fc6362
mrcpc <615706431@qq.com>
2018-01-11 17:21:33 +0800

    设置文本编码

--- learnp/basic/bupt_2017_12_04/Property.py
+++ learnp/basic/bupt_2017_12_04/Property.py
@@ -10,7 +10,7 @@ Time:14:20
 class Property:
     def __init__(self, path: str, sep: str = '='):
         self.d = {}
-        with open(path) as f:
+        with open(path,encoding='utf-8') as f:
             key_vals = map(lambda x: re.sub(r'\s+', '', x), f.readlines())
             self.d = {key_val.split(sep)[0]: key_val.split(sep)[1] for key_val in key_vals if key_val != ''}
 
