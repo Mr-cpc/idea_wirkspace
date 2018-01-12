72e13ba10071f2f30a468a68eb6d1577e1370fb9
mrcpc <615706431@qq.com>
2018-01-12 11:22:12 +0800

    相对来说比较完整的版本，可以自动爬分页

---
+++ learnp/basic/bupt_2018_1_11/doubleharm.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from pip._vendor import requests as req
+
+from basic.bupt_2017_12_04.Property import Property
+from collections import Counter
+import re
+import threading
+import time
+ppt = Property('global.properties')
+# ([a-zA-Z0-9%_.\-]*)
+# shijie_url = "http://xyq-ios2.cbg.163.com/app2-cgi-bin//xyq_search.py?orderby=selling_time%20DESC&act=super_query&search_type=overall_lingshi_search&equip_level_min=80&equip_level_max=100&kindid=61&damage=1&added_attr_logic=and&added_attr=2&added_attr_repeat_num=2&serverid=681&page=1&platform=ios&app_version=2.3.2&device_name=My%20iPhone&os_name=iPhone%20OS&os_version=9.3.3&device_id=10272229-E368-4457-95B9-427D8EEDA461"
+
+cookies = {"device_id":"10272229-E368-4457-95B9-427D8EEDA461", "sid":"Ieqh_xBURWcRSFIXx_mv80c1cQoj9pY6PZP5U1Ib", "NTES_SESS":"htGIQEPHahQqM5WvOLSUa7WxTZgng2S1mMOeyBLpy8lDBhbjjfx80JKEDDF7FBf4_rqVLnr9t1POyRnauOas06dmfqE9glNk87h7NkpsFU9tdd0lriXv1dTFh7weY5dqYFOdhGvVOpVeLrfc8NH7N7lmzr3Nm4hQAOEcNkNRLlLo4Q84JiG.PP9lY2siR3oyyyCkZvgC95RQ9PrA.7yZm2cIZ", "_ga":"GA1.2.2096250666.1513858580", "_ntes_nnid":"ecaac034c11f75726577b33dbb2c28ae,1514797131942", "_ntes_nuid":"ecaac034c11f75726577b33dbb2c28ae"}
+ppt = Property('global.properties')
+def query():
+    quanbu_url = 'http://xyq-ios2.cbg.163.com/app2-cgi-bin//xyq_search.py?orderby=selling_time%20DESC&act=super_query&search_type=overall_lingshi_search&equip_level_min=80&equip_level_max=100&kindid=61&damage=1&added_attr_logic=and&added_attr=2&added_attr_repeat_num=2&server_type=3&page=8&platform=ios&app_version=2.3.2&device_name=My%20iPhone&os_name=iPhone%20OS&os_version=9.3.3&device_id=10272229-E368-4457-95B9-427D8EEDA461'
+    with open('src.info','w',encoding='utf-8') as f:
+        i = 1
+        while True:
+            r = req.get(quanbu_url,verify=False,cookies = cookies)
+            contents = r.json()[ppt['eq_list']]
+            if len(contents) > 0:
+                pattern = re.compile('^{}'.format(ppt['harm']))
+
+                print('第{}页，{}个'.format(i,len(contents)))
+                print(contents[0],file=f)
+                print('---------page{}'.format(i),file=f)
+                for content in contents:
+                    a = pattern.match(content[ppt['main_attr']])
+                    b= Counter(content[ppt['down']][0].split(' '))[ppt['harm']] >= 2
+                    if a and b:
+                        print('----\nname:{}---\nprice:{}----\nmain:{}-----\ndown:{}----\nqu:{}'.format(content['equip_name'],content['price_desc'],content[ppt['main_attr']],content[ppt['down']],content['server_name']),file=f)
+                quanbu_url = re.sub(r'(page=)(\d+)',matchcase(),quanbu_url)
+                i += 1
+            else:break
+
+def matchcase():
+    def replace(m):
+        return '{}{}'.format(m.group(1),int(m.group(2))+1)
+    return replace
+while True:
+
+    num = query()
+    time.sleep(100)
