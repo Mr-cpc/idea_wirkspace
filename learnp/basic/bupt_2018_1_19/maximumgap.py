b3d7be137d47d8b707ef3d909884945c291accf4
mrcpc <615706431@qq.com>
2018-01-19 11:18:31 +0800

    164. Maximum Gap .要求O(n)的space/time complexiety,桶排序是一种选择

---
+++ learnp/basic/bupt_2018_1_19/maximumgap.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from basic.bupt_2017_11_28.type_deco import prt
+import joblib
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from basic.bupt_2017_11_28.type_deco import prt
+import seaborn as sns
+
+'''
+User:waiting
+Date:2018-01-19
+Time:11:07
+'''
+def mxgap(nums:list):
+    if len(nums) < 2:
+        return 0
+    bucket = [0] * (max(nums) + 1)
+    for val in nums:
+        bucket[val] += 1
+    sorted_form = [val for val in bucket if val != 0]
+    ans = 0
+    for i in range(len(nums)-1):
+        ans = max(ans,sorted_form[i+1]-nums[i] if sorted_form[i+1] >= nums[i] else nums[i] - sorted_form[i+1])
+    return ans
