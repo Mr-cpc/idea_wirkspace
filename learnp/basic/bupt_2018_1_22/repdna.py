e7311f9dbde6347cacf9416902d7f23b5e48ae25
mrcpc <615706431@qq.com>
2018-01-22 17:16:38 +0800

    187. Repeated DNA sequences。基本思想就是map，只是对map的key进行二进制编码， 减小空间复杂度。10个char->1个int

--- learnp/basic/bupt_2018_1_22/repdna.py
+++ learnp/basic/bupt_2018_1_22/repdna.py
@@ -1,4 +1,42 @@
+from basic.bupt_2017_10_30.deco import time_statistic
+
+
+@time_statistic
+def repdna2(s:str):
+    from collections import defaultdict
+    d = defaultdict(int)
+    ans = []
+    for i in range(len(s)-9):
+        x = s[i:i+10]
+        d[x] += 1
+        if d[x] == 2:
+            ans.append(x)
+        else:
+            continue
+    return ans
 def repdna(s:str):
     return [s[i:i+10] for i in range(len(s)-9) if s[i:i+10] in s[i+10:]]
+def encode(s:str) -> int:
+    enc = 0
+    d = {'A':0,'C':1,'G':2,'T':3}
+    i = 0
+    for ch in s:
+        enc += (d[ch] << i)
+        i += 2
+        enc <<= i
+    return enc
+def repdna3(s:str):
+    from collections import defaultdict
+    d = defaultdict(int)
+    ans = []
+    for i in range(len(s)-9):
+        x = s[i:i+10]
+        y = encode(x)
+        d[y] += 1
+        if d[y] == 2:
+            ans.append(x)
+        else:
+            continue
+    return ans
 
-print(repdna("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"))
+print(repdna3("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"))
