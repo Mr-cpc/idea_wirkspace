82c0d72e9ae7747e0f39d8b09cbe2630ede762a4
mrcpc <615706431@qq.com>
2018-01-22 09:16:24 +0800

    居然把这个忘记了

---
+++ learnp/basic/bupt_2017_11_24/ppthelper.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from basic.bupt_2017_11_28.type_deco import prt
+import joblib
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from basic.bupt_2017_11_28.type_deco import prt
+import seaborn as sns
+
+from basic.bupt_2017_12_04.Property import Property
+
+'''
+User:waiting
+Date:2018-01-15
+Time:10:15
+'''
+
+class PptHelper:
+    def __init__(self,path = r'G:\idea_wirkspace\learnp\basic\bupt_2017_11_24\global.properties'):
+        self.ppt = Property(path)
+
+    def __getitem__(self, item):
+        return self.ppt[item]
