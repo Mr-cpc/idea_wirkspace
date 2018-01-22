4c0232ad3e4f2ca53c8f46ccbb6b6eefb88499ad
user.name <615706431@qq.com>
2018-01-22 21:30:41 +0800

    这一题的case太恶心了，特别是最后一个大数的除法.

--- learnp/basic/bupt_2018_1_19/mxpoontheline.py
+++ learnp/basic/bupt_2018_1_19/mxpoontheline.py
@@ -36,10 +36,12 @@ def mxpotontheline(points:list):
         ans = max(ans,len(v))
     return ans
 def cal_slope(p1,p2):
-    return (p1.y -p2.y) / (p1.x - p2.x) if p1.x != p2.x else float('inf')
+    return Decimal(p1.y -p2.y) / Decimal(p1.x - p2.x) if p1.x != p2.x else float('inf')
 def mxpotontheline2(points:list):
     if len(points) < 1:
         return 0
+    if len(points) == 2:
+        return 2
     ans = 1
     from collections import defaultdict
     for i in range(len(points)):
@@ -50,11 +52,17 @@ def mxpotontheline2(points:list):
                 same += 1
             else:
                 d[cal_slope(points[i],points[j])] += 1
+        if not d:
+            d[float('inf')] = 0
         for key in d:
             d[key] += same
+        print(d)
         ans = max(ans,max(d.values())+1) if d else ans
+
     return ans
-
-
-d = defaultdict(int)
-print(mxpotontheline2([Point(1,2),Point(2,3),Point(2,3),Point(1,2)]))
+if __name__ == '__main':
+    from decimal import Decimal
+    d = defaultdict(int)
+    print(mxpotontheline2([Point(0,0),Point(94911151,94911150),Point(94911152,94911151)]))
+    x = Decimal(94911150) /Decimal(94911151)
+    y = Decimal(949111500) /Decimal(949111510)
