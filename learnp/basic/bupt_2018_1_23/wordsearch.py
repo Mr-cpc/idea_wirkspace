07661c19704f3f052d693d658e8070fcf81c1bf7
mrcpc <615706431@qq.com>
2018-01-23 10:23:38 +0800

    212. Word Search II。现在这种在二维数组里的搜索基本都是dfs回溯，但是能不能ac 就要看剪枝的技术了

---
+++ learnp/basic/bupt_2018_1_23/wordsearch.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from basic.bupt_2017_11_28.type_deco import prt
+import joblib
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from basic.bupt_2017_11_28.type_deco import prt
+import seaborn as sns
+
+'''
+User:waiting
+Date:2018-01-23
+Time:9:42
+'''
+
+
+def dfs(board:list, pos:tuple,word:str,s:set):
+    if not word:
+        return True
+    elif pos in s:
+        return False
+    elif not (0 < pos[1] < len(board[0])) and (0 < pos[0] < len(board)):
+        return False
+    elif word[0] != board[pos[0]][pos[1]]:
+        return False
+    else:
+        for i in (-1,1):
+            s_copy1 = s.copy()
+            s_copy1.add(pos)
+            if dfs(board,(pos[0],pos[1]+i),word[1:],s_copy1):
+                return True
+            s_copy2 = s.copy()
+            s_copy2.add(pos)
+            if dfs(board,(pos[0]+i,pos[1]),word[1:],s_copy2):
+                return True
+        return False
+def contain(board:list, word:str):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if dfs(board,(i,j),word,set()):
+                return True
+    return False
+
+
+def wdsearch(board:list,words:list):
+    return [word for word in words if contain(board,word)]
+if __name__ == '__main__':
+    # board = [
+    #     ['o','a','a','n'],
+    #     ['e','t','a','e'],
+    #     ['i','h','k','r'],
+    #     ['i','f','l','v']
+    # ]
+    board = [[]]
+    print(bool(board))
+    # words = ["oath","pea","eat","rain"]
+    words = []
+    print(wdsearch(board,words))
+
+    
