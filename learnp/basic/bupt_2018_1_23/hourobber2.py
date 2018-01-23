d278258ecf4ce3ce918350395369859cef356c6c
user.name <615706431@qq.com>
2018-01-23 20:48:48 +0800

    这是ac版本，之前下标写错了

--- learnp/basic/bupt_2018_1_23/hourobber2.py
+++ learnp/basic/bupt_2018_1_23/hourobber2.py
@@ -15,14 +15,20 @@ Date:2018-01-23
 Time:10:34
 '''
 def hourob2(nums:list):
+    if not nums:
+        return 0
+    if len(nums) in (1,2):
+        return max(nums)
     sub1,sub2 = nums[1:],nums[:-1]
+    print(sub1)
+    print(sub2)
     dp1,dp2 = [0] * len(nums),[0] * len(nums)
-    dp1[1],dp2[0] = sub1[0],sub2[0]
+    dp1[1],dp2[1] = sub1[0],sub2[0]
     for i in range(2,len(dp1)):
         dp1[i] = max(dp1[i-1],dp1[i-2]+sub1[i-1])
         dp2[i] = max(dp2[i-1],dp2[i-2]+sub2[i-1])
     return max(dp1[-1],dp2[-1])
 if __name__ == '__main__':
-    print(hourob2([]))
+    print(hourob2([1,1,1]))
     pass
     
