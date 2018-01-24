3ddb04931ce48492d34db1a5993ecc72ea29efb9
mrcpc <615706431@qq.com>
2018-01-24 10:46:18 +0800

    为什么这个Trie版本如果用list来装，会有重复？

--- learnp/basic/bupt_2018_1_23/wordsearch.py
+++ learnp/basic/bupt_2018_1_23/wordsearch.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from basic.bupt_2017_11_06.lgwid import TrieNo
 from basic.bupt_2017_11_28.type_deco import prt
 import joblib
 from sklearn import preprocessing
@@ -32,6 +34,21 @@ def dfs(board:list, pos:tuple,word:str,s:set):
             if dfs(board,(pos[0]+i,pos[1]),word[1:],{*s,pos}):
                 return True
         return False
+def dfs2(board:list, pos:tuple,word:str,s:set,trie:TrieNo,ans:set):
+    if pos in s:
+        return
+    elif not ((0 <= pos[1] < len(board[0])) and (0 <= pos[0] < len(board))):
+        return
+    elif trie.contains(word):
+        ans.add(word)
+        return
+    elif not trie.starts_with(word):
+        return
+    else:
+        cur_word = '{}{}'.format(word,board[pos[0]][pos[1]])
+        for i in (1,-1):
+            dfs2(board,(pos[0],pos[1]+i),cur_word,{*s,pos},trie,ans)
+            dfs2(board,(pos[0]+i,pos[1]),cur_word,{*s,pos},trie,ans)
 def contain(board:list, word:str):
     for i in range(len(board)):
         for j in range(len(board[0])):
@@ -42,6 +59,16 @@ def contain(board:list, word:str):
 
 def wdsearch(board:list,words:list):
     return [word for word in set(words) if contain(board,word)]
+
+def wdsearch_use_trie(board:list,words:list):
+    trie = TrieNo(' ')
+    ans = set()
+    for word in words:
+        trie.insert(word)
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            dfs2(board,(i,j),'',set(),trie,ans)
+    return list(ans)
 if __name__ == '__main__':
     board = [
         ['o','a','a','n'],
@@ -49,11 +76,11 @@ if __name__ == '__main__':
         ['i','h','k','r'],
         ['i','f','l','v']
     ]
-    board = [['a']]
+    # board = [['a']]
     # print(bool(board))
 
     words = ["a",'a']
-    # words = []
-    print(wdsearch(board,words))
+    words = ["oath","pea","eat","rain"]
+    print(wdsearch_use_trie(board,words))
 
     
