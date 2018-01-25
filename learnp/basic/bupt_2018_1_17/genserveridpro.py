c0aeef8a57c8922529ac7b7c7cfaba5c35af1266
mrcpc <615706431@qq.com>
2018-01-25 15:29:23 +0800

    update

--- learnp/basic/bupt_2018_1_17/genserveridpro.py
+++ learnp/basic/bupt_2018_1_17/genserveridpro.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from basic.bupt_2017_10_30.deco import time_statistic
 from basic.bupt_2017_11_24.ppthelper import PptHelper
 from basic.bupt_2017_11_28.type_deco import prt
 import joblib
@@ -19,13 +20,14 @@ Date:2018-01-17
 Time:10:07
 '''
 def genserveridpro():
-    with open(r'G:\idea_wirkspace\learnp\basic\bupt_2018_1_16\serverid.txt',encoding='utf-8') as f:
+    with open(r'G:\idea_wirkspace\learnp\basic\bupt_2018_1_16\qu.txt',encoding='utf-8') as f:
         d = {x[1:-2].split(',')[2]:x[1:-2].split(',')[0] for x in f}
-    with open('serverid.properties','w',encoding='utf-8') as f:
+    with open('serverid2.properties','w',encoding='utf-8') as f:
         for k,v in d.items():
             print('{}={}'.format(k[2:-1],v),file=f)
 
 # genserveridpro()
+@time_statistic
 def query(serv_name:str):
     ppt = PptHelper()
     ppt_servid = PptHelper('serverid.properties')
@@ -38,6 +40,23 @@ def query(serv_name:str):
     import basic.bupt_2018_1_11.doubleharm
     basic.bupt_2018_1_11.doubleharm.query(url,'{}.txt'.format(serv_name))
 
+def update_ppt(f1:str,f2:str):
+    '''
+    将f2中有但是f1中没有的内容添加到f1中
+    :param f1: str
+    :param f2: str
+    :return: 
+    '''
+    if not (f1.endswith('properties') and f2.endswith('properties')):
+        raise Exception('not properties file!')
+    ppt1 = PptHelper(f1)
+    ppt2 = PptHelper(f2)
+    s1 = {(x,y) for x,y in ppt1.items()}
+    s2 = {(x,y) for x,y in ppt2.items()}
+    add = s2 - s1
+    with open(f1,'a',encoding='utf-8') as f:
+        for pair in add:
+            f.write('{}={}\n'.format(pair[0],pair[1]))
 if __name__ == "__main__":
-    query('雨花台')
+    query('世界之窗')
 
