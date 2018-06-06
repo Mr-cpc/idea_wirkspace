6c987f573b90166daf42363769290505a78ed0a5
mrcpc <615706431@qq.com>
2018-06-06 16:47:33 +0800

    线程获取到ReentrantLock后并不会响应中断。

--- src/bupt_2018_6_6/InteruptedTest.java
+++ src/bupt_2018_6_6/InteruptedTest.java
@@ -1,25 +1,31 @@
 package bupt_2018_6_6;
 
 import java.util.Scanner;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by waiting on 2018/6/6.
  */
 public class InteruptedTest {
     public static void main(String[] args) {
-        test();
+        test1();
 
     }
-
+    private static boolean sleep(long mill) {
+        try {
+            Thread.sleep(mill);
+            return true;
+        } catch (InterruptedException e) {
+            System.out.println("be interrupted while sleeping!");
+            return false;
+        }
+    }
     private static void test() {
         Thread t = new Thread(()->{
             while (true) {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    System.out.println("be interrupted while sleeping!");
-                    break;
-                }
+//                if (!sleep(3000))
+//                    break;
             }
         });
         t.start();
@@ -28,6 +34,23 @@ public class InteruptedTest {
         while (!(line = sc.nextLine()).equals("exit") ) {
             System.out.printf("you input is %s\n",line);
         }
+        t.interrupt();
+    }
+    private static void test1() {
+        final Lock lock = new ReentrantLock();
+        Thread t = new Thread(()->{
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                lock.lock();
+                while (true) {
+                    if (1 == 2)
+                        break;
+                }
+                lock.unlock();
+            }
+
+        });
+        t.start();
         t.interrupt();
     }
 }
