42791daaba6fcaf0af9deaa91ccf06bf1908dc5a
mrcpc <615706431@qq.com>
2018-06-13 17:33:51 +0800

    最长有效匹配括号子串

---
+++ src/bupt_2018_6_12/LongPalin.java
@@ -0,0 +1,30 @@
+package bupt_2018_6_12;
+
+import java.util.Arrays;
+
+/**
+ * Created by waiting on 2018/6/12.
+ */
+public class LongPalin {
+    public static int longestValidParentheses(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return maxans;
+    }
+
+    public static void main(String[] args) {
+        String bra = "()((()))";
+        longestValidParentheses(bra);
+    }
+}
